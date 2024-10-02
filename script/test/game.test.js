967d9fda6605397d49162040fd1d9625cd9f4375
Cass Beckford <CassJBeckford282@outlook.com>
2024-10-02 17:29:37 +0000

    add and test newGame and showScore functions

--- script/test/game.test.js
+++ script/test/game.test.js
@@ -1,5 +1,5 @@
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -26,3 +26,22 @@ describe("game object contains correct keys", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should clear currentGame array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("id = 'score' should display 0", () => {
+        expect(document.getElementById('score').innerText).toBe(0);
+    });
+})
